a039abca7584ff87cd6ab3af82928d43553b09dc
giordifungula <jodi.fungula@gmail.com>
2019-02-22 23:01:00 +0200

    calculator javascript added

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-// Store Numbers into Variables
-let one = document.getElementById('one');
-let output = document.getElementById('output');
-let numberblocks = document.querySelectorAll('.item-block');
 
-numberblocks.forEach(function(e){
-    // if(e){
-        alert(e,'That is a number');
-    // }
-})
-console.log(numberblocks);
-one.addEventListener('click', function(){
-    output.textContent = one.textContent;
-});
+function element(e){
+    // returns back element or ID
+    return document.getElementById(e);
+}
+function display(value){
+    // Collections whatever element's value 
+    element('output').value += value;
+    // This will statically input the operands also
+}
+
+function refresh(value){
+    element('output').value = '';
+    // When AC is clicked remove value
+}
+
+function equal(){
+    let storedValue = element('output').value;
+    if(storedValue === '' || storedValue === undefined){
+        // Validate if user did not add numbers
+        alert("Please Enter a Number first");
+    } else {
+        element('output').value = eval(element('output').value);
+    }
+    // Calculates what ever was clicked 
+//    elementValue = eval(elementValue.value);
+    // clearCalculator();
+}
+
+function clearCalculator(){
+    // element('output').value = '';
+}
